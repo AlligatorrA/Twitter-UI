566232769c8f783be07f0c2e048d06969d2c22bb
Anand <anandraj143kick@gmail.com>
2022-01-19 15:29:10 +0530

    css

---
+++ script.js
@@ -0,0 +1,8 @@
+const ImageUpload = document.querySelector(".ImageUpload")
+const ImageUploadButton = document.querySelector(".ImageUploadButton")
+const backProfileImg = document.querySelector(".backProfileImg")
+const SearchSide = document.querySelector(".SearchSide")
+let listItems = document.querySelectorAll('ul > li > a');
+
+
+
